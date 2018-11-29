e8c4f8b55155284f2b50159e0cf97aa45a537a04
Paul Copley <paulcopley@Pauls-MacBook-Pro.local>
2018-11-29 09:48:36 +0000

    Update paths in utils for new dir structure

--- utils/intel-helpers.js
+++ utils/intel-helpers.js
@@ -1,57 +1,71 @@
 const faker = require('faker');
 const random = require('lodash.random');
-const {promisify} = require('util')
+const { promisify } = require('util');
 const fs = require('fs');
 const mkdir = promisify(fs.mkdir);
 const writeFile = promisify(fs.writeFile);
 
-
 const generateOwner = () => {
   return {
-    forename: faker.name.firstName(), 
-    surname: faker.name.lastName(), 
-    age: random(16, 120), 
-  }
+    forename: faker.name.firstName(),
+    surname: faker.name.lastName(),
+    age: random(16, 120)
+  };
 };
 
 const generateMultiples = (n, generator, limitCondition) => {
-  return Array.from({length: n}, () => generator(limitCondition));
+  return Array.from({ length: n }, () => generator(limitCondition));
 };
 
-const generateShop = (ownerCount) => {
+const generateShop = ownerCount => {
   return {
-    shop_name: faker.company.companyName(), 
+    shop_name: faker.company.companyName(),
     shop_owner: random(1, ownerCount),
     slogan: faker.company.catchPhrase()
-  }
+  };
 };
 
-const generateTreasure = (shopCount) => {
+const generateTreasure = shopCount => {
   return {
     treasure_name: `${faker.company.catchPhraseAdjective()} ${faker.commerce.productName()}`,
-    colour: faker.commerce.color(), 
-    age: random(5, 1000), 
-    cost_at_auction: random(0, 100000), 
+    colour: faker.commerce.color(),
+    age: random(5, 1000),
+    cost_at_auction: random(0, 100000),
     shop: random(1, shopCount)
   };
-}
+};
 
 const generateFileText = js => `module.exports = ${JSON.stringify(js)}`;
 
 module.exports = (ownerCount, shopCount, treasureCount) => {
   const owners = generateMultiples(ownerCount, generateOwner);
   const shops = generateMultiples(shopCount, generateShop, ownerCount);
-  const treasures = generateMultiples(treasureCount, generateTreasure, shopCount);
-  mkdir('./seed/intel')
+  const treasures = generateMultiples(
+    treasureCount,
+    generateTreasure,
+    shopCount
+  );
+  mkdir('./db/seed/intel')
     .catch(() => console.log('Overwriting existing files in intel directory'))
     .then(() => {
-      return writeFile('./seed/intel/owners.js', generateFileText(owners), 'utf8')
+      return writeFile(
+        './db/seed/intel/owners.js',
+        generateFileText(owners),
+        'utf8'
+      );
     })
     .then(() => {
-      return writeFile('./seed/intel/shops.js', generateFileText(shops), 'utf8')
+      return writeFile(
+        './db/seed/intel/shops.js',
+        generateFileText(shops),
+        'utf8'
+      );
     })
     .then(() => {
-      return writeFile('./seed/intel/treasures.js', generateFileText(treasures), 'utf8')
-    })
+      return writeFile(
+        './db/seed/intel/treasures.js',
+        generateFileText(treasures),
+        'utf8'
+      );
+    });
 };
-
