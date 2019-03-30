6abfaf6fb79ac5aeb3d525966f4d5896ee515a2c
Anthony Medina <antmedina91@gmail.com>
2019-03-30 19:13:11 +0000

    format files

--- db/test-setup.sql
+++ db/test-setup.sql
@@ -4,27 +4,27 @@ CREATE DATABASE mitchs_rare_treasures_test;
 \c mitchs_rare_treasures_test;
 
 CREATE TABLE owners (
-  owner_id SERIAL PRIMARY KEY, 
-  forename VARCHAR(255), 
-  surname VARCHAR(255), 
-  age INT NOT NULL, 
+  owner_id SERIAL PRIMARY KEY,
+  forename VARCHAR(255),
+  surname VARCHAR(255),
+  age INT NOT NULL,
   CHECK (age BETWEEN 16 AND 250)
 );
 
 
 CREATE TABLE shops (
-  shop_id SERIAL PRIMARY KEY, 
-  shop_name VARCHAR(255) NOT NULL, 
-  owner_id INT REFERENCES owners(owner_id) NOT NULL, 
+  shop_id SERIAL PRIMARY KEY,
+  shop_name VARCHAR(255) NOT NULL,
+  owner_id INT REFERENCES owners(owner_id) NOT NULL,
   slogan TEXT
 );
 
 
 CREATE TABLE treasures (
   treasure_id SERIAL PRIMARY KEY,
-  treasure_name VARCHAR(255) NOT NULL, 
-  colour VARCHAR(255) NOT NULL, 
-  age INT NOT NULL, 
+  treasure_name VARCHAR(255) NOT NULL,
+  colour VARCHAR(255) NOT NULL,
+  age INT NOT NULL,
   cost_at_auction NUMERIC(10, 2) NOT NULL,
   shop_id INT REFERENCES shops(shop_id) NOT NULL
 );
