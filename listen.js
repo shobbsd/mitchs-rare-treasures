5894652df845bb8be04ceb24b86847bd2313c00c
Shaq <shobbsdaley@gmail.com>
2019-10-12 12:30:00 +0100

    first test written for app

---
+++ listen.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+const PORT = process.env.PORT || 9090;
+
+app.listen(PORT);
---
+++ spec/app.spec.js
@@ -0,0 +1,31 @@
+const { expect } = require('chai');
+const request = require('supertest');
+const app = require('../app');
+const connection = require('../db/connection');
+
+describe('/api', () => {
+  beforeEach(() => {
+    return connection.seed.run();
+  });
+  after(() => {
+    connection.destroy();
+  });
+  describe('/treasures', () => {
+    it('GET:200 returns an array of treasures', () => {
+      return request(app)
+        .get('/api/treasures')
+        .expect(200)
+        .then(({ body: { articles } }) => {
+          expect(articles).to.be.an('array');
+          expect(articles[0]).to.have.keys([
+            'treasure_id',
+            'treasure_name',
+            'colour',
+            'age',
+            'cost_at_auction',
+            'shop_name'
+          ]);
+        });
+    });
+  });
+});
