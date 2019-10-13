d9282c9135f78ac3bfd55a485ff49e1c768e589b
Shaq <shobbsdaley@gmail.com>
2019-10-13 02:38:03 +0100

    adding error handling

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const apiRouter = require('./routes/api');
+const {
+  handleCustomErrors,
+  handlePsqlErrors,
+  handle500Errors
+} = require('./errors/');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api', apiRouter);
+
+app.use(handleCustomErrors);
+app.use(handlePsqlErrors);
+app.use(handle500Errors);
 
 module.exports = app;
---
+++ errors/index.js
@@ -0,0 +1,40 @@
+exports.handleCustomErrors = (err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).json({ msg: err.msg });
+  } else next(err);
+};
+
+exports.handlePsqlErrors = (err, req, res, next) => {
+  const errRef = {
+    '42703': {
+      status: 400,
+      msg: createPsqlMessage(err)
+    },
+    '23502': {
+      status: 400,
+      msg: create23502Message(err)
+    },
+    '22P02': {
+      status: 400,
+      msg: createPsqlMessage(err)
+    }
+  };
+  const error = errRef[err.code];
+  if (error) {
+    res.status(error.status).json({ msg: error.msg });
+  } else next(err);
+};
+
+exports.handle500Errors = (err, req, res, next) => {
+  res
+    .status(500)
+    .json({ msg: 'uh oh, something serious has happened. Sorry!' });
+};
+
+function createPsqlMessage(err) {
+  return err.message.split(' - ')[1];
+}
+
+function create23502Message(err) {
+  return `${err.column} is missing from insertion`;
+}
