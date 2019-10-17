92d93c567694aa697bb47a961fb4cc6a29edaf4d
Shaq <shobbsdaley@gmail.com>
2019-10-17 13:27:05 +0100

    extracting util functions from shops.js

--- controllers/shops.js
+++ controllers/shops.js
@@ -1,28 +1,15 @@
 const { fetchShops } = require('../models/shops');
+const { checkSortBys } = require('./utils');
 
 exports.getShops = (req, res, next) => {
   const { sort_by, order, p, limit } = req.query;
   const acceptableSorts = ['shop_name', 'owner', 'stock_value'];
 
-  if (sort_by && !acceptableSorts.includes(sort_by))
-    return next({
-      status: 400,
-      msg: 'sort_by mut be either shop_name, owner or stock_value'
-    });
-
-  if (order && !['asc', 'desc'].includes(order))
-    return next({
-      status: 400,
-      msg: 'order mut be either asc or desc'
-    });
-
-  if (p && isNaN(+p))
-    return next({
-      status: 400,
-      msg: 'p mut be a number'
-    });
-
-  fetchShops(sort_by, order, limit, p).then(shops => {
-    res.status(200).json({ shops });
-  });
+  if (checkSortBys(req.query, acceptableSorts, next)) {
+    fetchShops(sort_by, order, limit, p)
+      .then(shops => {
+        res.status(200).json({ shops });
+      })
+      .catch(next);
+  }
 };
